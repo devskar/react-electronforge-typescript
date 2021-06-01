bc904f64ed22c8bfafce7388e4553bf986f7b661
oskar <ooskar@gmx.de>
2021-06-01 22:28:08 +0200

    Added App.tsx and folders

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {}
+
+const App: React.FC<Props> = () => {
+  return (
+    <>
+      <h1>Hello world from React!</h1>
+    </>
+  );
+};
+
+export default App;
