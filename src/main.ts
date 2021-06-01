04d34de354dc8a9964d4dbec00bf389d4e3d3495
oskar <ooskar@gmx.de>
2021-06-01 19:08:11 +0200

    added electron-is-dev

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { app, BrowserWindow } from 'electron';
+import * as isDev from 'electron-is-dev';
 declare var MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-  // eslint-disable-line global-require
   app.quit();
 }
 
@@ -26,7 +26,7 @@ const createWindow = () => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if (isDev) mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
