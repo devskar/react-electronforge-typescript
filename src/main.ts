896a066258a7be912cbff1d6430ab2e5c8a0ce5a
devskar <ooskar@gmx.de>
2021-06-02 22:16:14 +0200

    removed unnecassary console log and test ipc

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import isDev from 'electron-is-dev';
 declare var MAIN_WINDOW_WEBPACK_ENTRY: any;
 declare var MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: any;
 
-console.log(MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY);
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -65,6 +63,3 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
-
-// test ipc
-ipcMain.on('test', (event, message) => console.log(message));
--- src/renderer/preload.ts
+++
@@ -1,4 +0,0 @@
-import { ipcRenderer } from 'electron';
-
-// test ipc
-ipcRenderer.send('test', 'ping');
